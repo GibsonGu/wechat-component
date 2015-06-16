d6af5ac2c0c2bbb19b7d118bdeb5161e657ee8ad
Gibson <bright.gu@gmail.com>
2015-06-16 17:33:29 +0800

    修改handleevent的路由访问方式

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,6 @@
 
 Route::group(['prefix'=>'wechat', 'namespace'=>'Gibson\Wechat\Controllers'], function()
 {
-	Route::get('handleevent', ['uses'=>'WechatController@handleEvent']);
+	Route::any('handleevent', ['uses'=>'WechatController@handleEvent']);
 	Route::get('authorize', ['uses'=>'WechatController@authorize']);
 });
