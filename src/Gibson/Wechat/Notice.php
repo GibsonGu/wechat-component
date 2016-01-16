fb538554c14bb44f11dc983b93ed5e94877a38ee
Gibson <bright.gu@gmail.com>
2016-01-16 14:27:43 +0800

    添加获取账号所属行业  overtrue/wechat": "^2.1"

--- src/Gibson/Wechat/Notice.php
+++ src/Gibson/Wechat/Notice.php
@@ -2,6 +2,7 @@
 
 class Notice extends \Overtrue\Wechat\Notice
 {
+    const API_GET_INDUSTRY = 'https://api.weixin.qq.com/cgi-bin/template/get_industry';
 
     /**
      * @param string|AccessToken $accessToken
@@ -9,5 +10,18 @@ class Notice extends \Overtrue\Wechat\Notice
     public function __construct($accessToken)
     {
         $this->http = new Http($accessToken);
+    }
+
+    /**
+     * 获取账号所属行业
+     *
+     * @param int $industryOne
+     * @param int $industryTwo
+     *
+     * @return bool
+     */
+    public function getIndustry()
+    {
+        return $this->http->jsonPost(self::API_GET_INDUSTRY);
     }
 }
